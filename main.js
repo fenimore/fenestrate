42ff6b0ac5ef361786734f82267c56f4f70282c3
polypmer <exorable.ludos@gmail.com>
2016-09-28 16:23:39 -0400

    Slightly working prototype fuck yea

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+var info = document.createElement('div');
+
 document.addEventListener('mousemove', function(e) {
+    info.textContent = document.elementFromPoint(e.pageX, e.pageY);
+    document.elementFromPoint(e.pageX, e.pageY).appendChild(info);
     //console.log(document.elementFromPoint(e.pageX, e.pageY));
 });
 
-function test() {
-    console.log("hello");    
-}
 
-chrome.browserAction.onClicked.addListener(test);
+
+
+
+
