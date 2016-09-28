da64e1d82ee993b31384782b2f3d1b8d63987e5c
polypmer <exorable.ludos@gmail.com>
2016-09-28 18:01:59 -0400

    Absolute position follows the mouse, looookin good

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-var info = document.createElement('div');
+var info = document.createElement('code');
 
-// Plug prism css into page:
-var file = location.pathname.split( "/" ).pop();
-
-var link = document.createElement( "link" );
-link.href = file.substr(0, file.lastIndexOf( "." ) ) + ".css";
-link.type = "text/css";
-link.rel = "stylesheet";
-link.media = "screen,print";
-
-document.getElementsByTagName( "head" )[0].appendChild( link );
 
 
 
 document.addEventListener('mousemove', function(e) {
-    //var test = document.elementFromPoint(e.pageX, e.pageY);
-    //var test2 = test.textContent + ;
-    //test.textContent = "...";
+
     var elem = document.elementFromPoint(e.pageX, e.pageY);
     var txt = elem.outerHTML.replace(elem.innerHTML, '...');
     info.textContent = txt;
-    //console.log(txt);
-    //info.textContent = document.elementFromPoint(e.pageX, e.pageY);
+    info.style = "background-color:#fdf6e3;color:#657b83;position:fixed;";
+    info.style.left = e.pageX + "px";
+    info.style.top = e.pageY + "px";
     document.elementFromPoint(e.pageX, e.pageY).appendChild(info);
     //console.log(document.elementFromPoint(e.pageX, e.pageY));
 });
