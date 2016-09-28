a17bf78482157f58d4a873b24cbebe07a8f5a258
polypmer <exorable.ludos@gmail.com>
2016-09-28 15:37:00 -0400

    Failure to do anything chrome related

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var currentTab;
 
 function doItsThing() {
     // requires the "tabs" or "activeTab" permission
-    
+    chrome.tabs.executeScript(null, {
+	file: "/content_scripts/log.js"
+    }    
     console.log("hello");
 
 };
