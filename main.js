69c0bff91e8255ef110105785a683ea6c9505552
polypmer <exorable.ludos@gmail.com>
2016-09-28 17:08:12 -0400

    Works thanks @haroldtreen

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var info = document.createElement('div');
 
 document.addEventListener('mousemove', function(e) {
-    info.textContent = document.elementFromPoint(e.pageX, e.pageY).innerHTML;
+    //var test = document.elementFromPoint(e.pageX, e.pageY);
+    //var test2 = test.textContent + ;
+    //test.textContent = "...";
+    var elem = document.elementFromPoint(e.pageX, e.pageY);
+    var txt = elem.outerHTML.replace(elem.innerHTML, '...');
+    info.textContent = txt;
+    //console.log(txt);
+    //info.textContent = document.elementFromPoint(e.pageX, e.pageY);
     document.elementFromPoint(e.pageX, e.pageY).appendChild(info);
     //console.log(document.elementFromPoint(e.pageX, e.pageY));
 });
