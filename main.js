a0b0660f878869578a78b03dce2e04341bdbe24a
polypmer <exorable.ludos@gmail.com>
2016-09-28 18:06:45 -0400

    Absolute position was overlapping the mouse woops, Fixed

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ var info = document.createElement('code');
 
 
 document.addEventListener('mousemove', function(e) {
-
+    var cornerX = e.pageX + 10;
+    var cornerY = e.pageY + 10;
+    
     var elem = document.elementFromPoint(e.pageX, e.pageY);
     var txt = elem.outerHTML.replace(elem.innerHTML, '...');
     info.textContent = txt;
     info.style = "background-color:#fdf6e3;color:#657b83;position:fixed;";
-    info.style.left = e.pageX + "px";
-    info.style.top = e.pageY + "px";
+    info.style.left = cornerX + "px";//e.pageX + "px";
+    info.style.top = cornerY + "px";//e.pageY + "px";
     document.elementFromPoint(e.pageX, e.pageY).appendChild(info);
-    //console.log(document.elementFromPoint(e.pageX, e.pageY));
 });
 
 
