24730d57b0dade80c7207d71933371cc4f601bdc
polypmer <exorable.ludos@gmail.com>
2016-09-29 10:23:30 -0400

    Added child node to view

--- content_scripts/magnify.js
+++ content_scripts/magnify.js
@@ -1,36 +1,45 @@
 var magnify = true;
-var info = document.createElement('code');
+var info = document.createElement('div');
 var txt;
 
 
-document.addEventListener('mousemove', function(e) {
-    if (magnify) {
-	var cornerX = e.pageX + 10;
-	var cornerY = e.pageY + 10;
+if (magnify) {  
+    document.addEventListener('mousemove', function(e) {
+	if (magnify) {
+	    var cornerX = e.pageX + 10;
+	    var cornerY = e.pageY + 10;
+	    
+	    var elem = document.elementFromPoint(e.pageX, e.pageY);
+	    var clone = elem.cloneNode(true);
+	    var child = clone.childNodes[0];
+	    var childTxt = "";
+	    //children[0].textContent = "...";
+	    if (child.nodeType === 1){
+		childTxt += "\n  " + child.outerHTML + "\n";		
+	    }
+	    clone.textContent = childTxt;
+	    var txt = clone.outerHTML;
+
+	    
+	    info.textContent = txt;
+	    // STYLE
+	    info.style = "position:fixed;background: #eee; padding: 1em; color: #000; font-family: monospace;white-space:pre-wrap;";
+	    // Failed STYLE
+	    //info.style = "background-color:#fdf6e3;color:#657b83;position:fixed;border-radius:50%;border:1px solid #657b83;";
+	    // more Failed STYLE
+	    // height: 200px;width: 200px;vertical-align: middle;padding:20px;
+	    info.style.left = "0px";//cornerX + "px";
+	    info.style.top = cornerY + "px";
+	    //info.style.visibility = "visible";
 	
-	var elem = document.elementFromPoint(e.pageX, e.pageY);
-	if (elem){
-	    var txt = elem.outerHTML.replace(elem.innerHTML, '...');
-	    console.log("true!");
-	} else {
-	    var txt = "<>...<>";
-	    console.log("false!");	    
+	    document.body.appendChild(info);
+	    
+	    //magnify = false;
 	}
-	info.textContent = txt;
-	info.style="position:fixed;";
-	//info.style = "background-color:#fdf6e3;color:#657b83;position:fixed;border-radius:50%;border:1px solid #657b83;";
-	// failed styles:
-	// height: 200px;width: 200px;vertical-align: middle;padding:20px;
-	info.style.left = "0px";//cornerX + "px";
-	info.style.top = cornerY + "px";
-	info.style.visibility = "visible";
-	
-	document.body.appendChild(info);
-	
-	magnify = false;
-    } else {
-	info.style.visibility = "hidden";
-	magnify = true;
-    }
-});
+    });
+    magnify == !magnify;
+} else {
+    info.style.visibility = "hidden";
+    magnify = true;
+}
 
