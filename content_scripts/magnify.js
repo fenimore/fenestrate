5ae71eceb1fe3900f6fe67b330b95b31dbbf8f2b
polypmer <exorable.ludos@gmail.com>
2016-09-29 08:35:34 -0400

    Removed silly style, circleness and following mouse.

--- content_scripts/magnify.js
+++ content_scripts/magnify.js
@@ -1,6 +1,6 @@
 var magnify = true;
 var info = document.createElement('code');
-
+var txt;
 
 
 document.addEventListener('mousemove', function(e) {
@@ -9,16 +9,23 @@ document.addEventListener('mousemove', function(e) {
 	var cornerY = e.pageY + 10;
 	
 	var elem = document.elementFromPoint(e.pageX, e.pageY);
-	var txt = elem.outerHTML.replace(elem.innerHTML, '...');
+	if (elem){
+	    var txt = elem.outerHTML.replace(elem.innerHTML, '...');
+	    console.log("true!");
+	} else {
+	    var txt = "<>...<>";
+	    console.log("false!");	    
+	}
 	info.textContent = txt;
-	info.style = "background-color:#fdf6e3;color:#657b83;position:fixed;border-radius:50%;border:1px solid #657b83;";
+	info.style="position:fixed;";
+	//info.style = "background-color:#fdf6e3;color:#657b83;position:fixed;border-radius:50%;border:1px solid #657b83;";
 	// failed styles:
 	// height: 200px;width: 200px;vertical-align: middle;padding:20px;
-	info.style.left = cornerX + "px";
+	info.style.left = "0px";//cornerX + "px";
 	info.style.top = cornerY + "px";
 	info.style.visibility = "visible";
 	
-	document.elementFromPoint(e.pageX, e.pageY).appendChild(info);
+	document.body.appendChild(info);
 	
 	magnify = false;
     } else {
