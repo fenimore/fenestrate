32d0473ccb77d17f9bfe32dd16c2cb38c5571ef7
polypmer <exorable.ludos@gmail.com>
2016-09-29 23:29:06 -0400

    failure to get highlight js to work

--- background.js
+++ background.js
@@ -2,6 +2,9 @@
 function toggleMag() {
     // code below is supposed to be inside your button trigge
     chrome.tabs.executeScript(null, {
+ 	file: "/content_scripts/highlight.pack.js"
+    });
+    chrome.tabs.executeScript(null, {
 	file: "/content_scripts/magnify.js"
     });
 }
